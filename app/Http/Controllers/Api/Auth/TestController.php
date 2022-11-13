0590488a942791bea0d6f9d38b18816504dfc40f
Sudip modi <sudipinshunya@gmail.com>
2022-11-14 00:26:04 +0530

    update

--- app/Http/Controllers/Api/Auth/TestController.php
+++ app/Http/Controllers/Api/Auth/TestController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api\Auth;
+
 use App\Http\Controllers\Controller;
 use App\Models\Extension;
 use App\Models\UserLogin;
@@ -13,26 +14,9 @@ use Illuminate\Support\Facades\Mail;
 
 class TestController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-
-    protected $username;
 
     /**
      * Create a new controller instance.
@@ -43,98 +27,71 @@ class TestController extends Controller
 
     public function __construct()
     {
-        $this->username = $this->findUsername();
+      
     }
-    
-    // calls find username behind th hood
-    public function username()
-    {
-        return $this->username;
-    }
+
 
     public function testValidate(Request $request)
     {
-        // the base url in this case will be
-        $testUrl = "https://staging.slotegrator.com/api/index.php/v1/self-validate";
-        //   the merchant id
-        $merchantId = 'ae88ab8ee84ff40a76f1ec2e0f7b5caa';
-        //   the merchant key
-        $merchantKey = '4953e491031d3f9e7545223885cf43a7403f14cb';
-        //   nonce
-        $nonce = md5(uniqid(mt_rand(), true));
-        //   current time
-        $time = time();
-
-        //   assign headers to the rquest , remember you are sending a request from the server to the staging url that slotegrator provided, authorization headers
-        $headers = [
-            'X-Merchant-Id' => $merchantId, 
-            'X-Timestamp' => $time,
-            'X-Nonce' => $nonce
-        ];
-
-        //   request parameters
-        $requestParams = [
-            'game_uuid' => $request->game_uuid,
-            'player_id' => $request->player_id, 
-            'currency' => 'USD',
-            'player_name' => $request->player_name,
-            'session_id' => session_id()
-        ];
-        //   mergerd params, merge request array with authorization headers array
-        $mergedParams = array_merge($requestParams, $headers);
-        //   sort by key in asc order
-        ksort($mergedParams);
-        //   generate hashstring, urlencoded query string
-        $queryString = http_build_query($mergedParams);
-
-        // echo $queryString;
-
-        //create sha 1 hmac hash using hashstring and merchanykey
-        $XSign = hash_hmac('sha1', $queryString, $merchantKey);
-
-        //   sort again
-        // ksort($requestParams);
-
-        // $postdata = http_build_query($requestParams);
-
-        $getHeader = array(
-            'X-Merchant-Id: ' . $merchantId,
-             'X-Timestamp: ' . $time,
-            'X-Nonce: ' . $nonce,
-            'X-Sign: ' . $XSign,
-            'Accept: application/json',
-            'Enctype: application/x-www-form-urlencoded'
-        );
-        // initialize curl request 
-        $ch = curl_init();
-        //   set url
-        curl_setopt($ch, CURLOPT_URL, $testUrl);
-        //   timeout value
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-        /*curl_setopt($ch, CURLOPT_POST, 1);*/
-        //   headers
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $getHeader);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        //   store result
-        $result = curl_exec($ch);
+        // $testUrl = "https://staging.slotegrator.com/api/index.php/v1/self-validate";
+        // $merchantId = 'ae88ab8ee84ff40a76f1ec2e0f7b5caa';
+        // $merchantKey = '4953e491031d3f9e7545223885cf43a7403f14cb';
+        // $nonce = md5(uniqid(mt_rand(), true));
+        // $time = time();
+        // $headers = [
+        //     'X-Merchant-Id' => $merchantId,
+        //     'X-Timestamp' => $time,
+        //     'X-Nonce' => $nonce
+        // ];
+        // $requestParams = [
+        //     'game_uuid' => $request->game_uuid,
+        //     'player_id' => $request->player_id,
+        //     'currency' => 'USD',
+        //     'player_name' => $request->player_name,
+        //     'session_id' => session_id()
+        // ];
+        // $mergedParams = array_merge($requestParams, $headers);
+        // ksort($mergedParams);
+        // $queryString = http_build_query($mergedParams);
+        // $XSign = hash_hmac('sha1', $queryString, $merchantKey);
 
 
-        if ($result === false) {
-            echo 'Curl error: ' . curl_error($ch);
-        } else {
-            echo 'Operation completed without any errors';
-        }
+        // $getHeader = array(
+        //     'X-Merchant-Id: ' . $merchantId,
+        //     'X-Timestamp: ' . $time,
+        //     'X-Nonce: ' . $nonce,
+        //     'X-Sign: ' . $XSign,
+        //     'Accept: application/json',
+        //     'Enctype: application/x-www-form-urlencoded'
+        // );
+
+        // $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL, $testUrl);
+        // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        // curl_setopt($ch, CURLOPT_HTTPHEADER, $getHeader);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // $result = curl_exec($ch);
+        // if ($result === false) {
+        //     echo 'Curl error: ' . curl_error($ch);
+        // } else {
+        //     echo 'Operation completed without any errors';
+        // }
+
+        // return response()->json([
+        //     'code' => 200,
+        //     'status' => 'ok',
+        //     'message' => [
+        //         'result' => $result,
+        //         'headers' => $headers,
+        //         'requestParamsHeaders' => $requestParams,
+        //         'hashstring' => $queryString,
+        //         'getDataHeader' => $getHeader
+        //     ]
+        // ]);
 
         return response()->json([
             'code' => 200,
             'status' => 'ok',
-            'message' => [
-                'result' => $result,
-                'headers' => $headers,
-                'requestParamsHeaders' => $requestParams,
-                'hashstring' => $queryString,
-                'getDataHeader' => $getHeader
-            ]
         ]);
-    }    
+    }
 }
